04e06f9f3a7978563f76c17a3f9d3f12b5b3651a
Badal Moradia <badal@i360llc.com>
2018-06-13 08:12:42 -0500

    Commit 9 - Created loops.js file

---
+++ loops.js
@@ -0,0 +1,35 @@
+//Create loop to log numbers 0-99
+for (let i = 0; i < 100; i++) {
+  console.log(`The value of i is: ${i}`);
+}
+
+//Create loop to log numbers from 99-0
+for (let j = 99; j >= 0; j--) {
+  console.log(`The value of j is: ${j}`);
+}
+
+//Create loop to log even numbers from 0-98
+for (let k = 0; k < 99; k += 2) {
+console.log(`The value of k is: ${k}`);
+}
+
+//Create loop to log even number from 98-0
+for (let l = 98; l >= 0; l -= 2) {
+  console.log(`The value of l is: ${l}`);
+}
+
+//Create loop to log numbers from 49-72
+for (let m = 49; m <= 72; m++) {
+  console.log(`The value of m is: ${m}`);
+}
+
+//Create loop to log numbers from 81-26
+for (let n = 81; n >= 26; n--) {
+  console.log(`The value of n is: ${n}`);
+}
+
+//Create loop to log numbers from 3-90 that are multiples of 3
+for (let o = 3; o <= 90; o += 3) {
+  console.log(`The value of o is: ${o}`);
+}
+
